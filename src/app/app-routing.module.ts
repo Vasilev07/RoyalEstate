6fed46c27c9dbebfcf386f357249e129aa43f8e2
Vasilev07 <georgevasilev07@gmail.com>
2021-01-18 23:11:49 +0200

    moved estate-details to estates folder

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -17,7 +17,7 @@ const routes: Routes = [
   },
   {
     path: 'estate-details/:id',
-    loadChildren: () => import('./estate-details/estate-details/estate-details.module').then( m => m.EstateDetailsPageModule)
+    loadChildren: () => import('../app/estates/estates/estate-details/estate-details/estate-details.module').then( m => m.EstateDetailsPageModule)
   }
 ];
 @NgModule({
