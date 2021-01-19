2e7797620d8a2b6ea1bcf9f43750d63b3434ed99
Vasilev07 <georgevasilev07@gmail.com>
2021-01-19 18:42:17 +0200

    fixed my estates view and find location button navigation

--- src/app/home/home/home.page.ts
+++ src/app/home/home/home.page.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { ToastController } from '@ionic/angular';
 import { Storage } from '@ionic/storage';
+import { IEstate } from 'src/app/common/interfaces/estate.interface';
+import { IEstates } from 'src/app/common/interfaces/estates.iterface';
 
 @Component({
   selector: 'app-home',
@@ -7,16 +11,34 @@ import { Storage } from '@ionic/storage';
   styleUrls: ['./home.page.scss'],
 })
 export class HomePage implements OnInit {
-  private estates = [];
-  constructor(private readonly storage: Storage) { }
+  private estates: IEstates[] = [];
+
+  constructor(private readonly storage: Storage,
+              private readonly router: Router,
+              private readonly toastController: ToastController) { }
 
   public async ngOnInit(): Promise<void> {
     try {
       this.estates = [...await this.storage.get('savedEstate')];
-      console.log(this.estates);
     } catch (error) {
-      
+      this.createToastForError('Error while fetching your saved estates.');
     }
   }
 
+  public onEstateClick(id: string) {
+    this.router.navigate(['/locations']);
+  }
+
+  public onEstateViewClick(estate: IEstate) {
+    this.router.navigate([`/estate-details/${estate.id}/overview`]);
+  }
+
+  private async createToastForError(message: string): Promise<void> {
+    const toast = await this.toastController.create({
+      message: 'Your settings have been saved.',
+      position: 'top',
+      duration: 2000
+    });
+    toast.present();
+  }
 }
